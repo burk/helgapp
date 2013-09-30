7ec4a4784e9996600b318be3d70a103e180ed928
Bjørn Rustad <bjorn@rustad.me>
2013-09-30 21:47:21 +0200

    Remove lame template and add progress bar

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from datetime import datetime
+import time
 
 def index(request):
-    return render(request, 'landingpage/index.html')
+    fmt = '%Y-%m-%d %H:%M:%S'
+    now   = datetime.now()
+    start = datetime.strptime('2013-08-01 12:00:00', fmt)
+    end   = datetime.strptime('2013-10-07 12:00:00', fmt)
+
+    now   = time.mktime(now.timetuple())
+    start = time.mktime(start.timetuple())
+    end   = time.mktime(end.timetuple())
+
+    return render(request, 'landingpage/index.html',
+            {'progress': 100 * (end - now) / (end - start)}
+    )
 
