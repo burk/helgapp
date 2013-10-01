b8f9c67f9a535c6cf74b035e9063b5a846c033b1
Bjørn Rustad <bjorn@rustad.me>
2013-10-01 21:13:36 +0200

    snapp: make filename primary key

--- snapp/models.py
+++ snapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Snap(models.Model):
-    filename = models.CharField(max_length=128)
+    filename = models.CharField(primary_key=True, max_length=128)
     username = models.CharField(max_length=128)
     downloaded = models.DateTimeField(auto_now_add=True)
 
