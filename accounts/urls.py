ee8be61fd1b9fdaef3f11914e44921d497738f8a
Bjørn Rustad <bjorn@rustad.me>
2013-10-14 21:05:53 +0200

    Registration works, I think

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from accounts import views
 
 urlpatterns = patterns('',
-    url(r'^signup/$', views.register_form, name = 'register'),
+    url(r'^register/$', views.register, name = 'register'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
     )
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth.models import User
-
-def register_form(request):
-    return render(request, 'registration/sign_up.html')
 
 def register(request):
-    User.objects.create_user(request.username, request.password)
-    return render(request, 'registration/login.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/register.html", {
+        'form': form,
+    })
+
